3a885882c99acd18f961bc26632df9d5f282861e
kranthi-kumar-2004 <159746543+kranthi-kumar-2004@users.noreply.github.com>
2026-04-14 13:43:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import './App.css';
 import Home from "./user/components/Home";
 import UserLayout from "./user/components/UserLayout";
 import AdminLayout from "./admin/components/AdminLayout";
@@ -9,21 +9,26 @@ import ManageProducts from "./admin/components/ManageProducts";
 import ProductPage from "./user/components/ProductPage";
 
 function App(){
-return (<>
+return (
 <BrowserRouter>
     <Routes>
+
+        {/* USER ROUTES */}
         <Route path="/" element={<UserLayout/>}>
             <Route index element={<Home/>}/>
-            <Route path="/cart" element={<Cart/>}/>
-            <Route path="/product/:id" element={<ProductPage />} />
+            <Route path="cart" element={<Cart/>}/>
+            <Route path="product/:id" element={<ProductPage />} />
         </Route>
+
+        {/* ADMIN ROUTES */}
         <Route path="/admin" element={<AdminLayout/>}>
             <Route index element={<AdminDashboard/>}/>
-            <Route path="/admin/manage"element={<ManageProducts/>}/>
+            <Route path="manage" element={<ManageProducts/>}/>
         </Route>
+
     </Routes>
 </BrowserRouter>
-</>);
+);
 }
 
 export default App;
