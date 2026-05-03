1e06b09f0b2575c8e4059970e6704525b6c839e6
kranthi-kumar-2004 <159746543+kranthi-kumar-2004@users.noreply.github.com>
2026-05-03 19:24:13 +0530

    Delete src directory

--- src/user/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-function Loader(){
-    return(
-      <div className="loader-container">
-      <div className="spinner"></div>
-      <p>Loading...</p>
-    </div>  
-    );
-}
-export default Loader;
